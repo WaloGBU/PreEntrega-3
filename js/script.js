b3f1ad74797ff582a27397ac54eca76b4921970a
Waldir Bustamante <waldir_9312@hotmail.com>
2023-01-22 17:08:17 -0500

    Se agregaron imágenes a la carpeta img

--- js/script.js
+++ js/script.js
@@ -5,15 +5,34 @@ const productos = [
     { id: 04, marca: "Gibson", modelo: "SG Standard", precio: 4800, cantidad: 1, img: "./img/GibsonSGStandard.jpg" },
     { id: 05, marca: "Ibanez", modelo: "Steve Vai Edition", precio: 6000, cantidad: 1, img: "./img/IbanezSteveVai.png" },
     { id: 06, marca: "Paul Red Smith", modelo: "Mark Holcomb", precio: 6500, cantidad: 1, img: "./img/PRSMarkHolcomb.png" },
-    { id: 07, marca: "LTD", modelo: "EX200", precio: 3500, cantidad: 1, img: "./img/LTDEX200.png" }
+    { id: 07, marca: "LTD", modelo: "EX200", precio: 3500, cantidad: 1, img: "./img/LTDEX200.png" },
+    { id: 08, marca: "Epiphone", modelo: "AJ-220S", precio: 2500, cantidad: 1, img: "./img/EpiphoneAJ-220S.jpg" },
+    { id: 09, marca: "Fender", modelo: "FA-125", precio: 3000, cantidad: 1, img: "./img/FenderFA125.png" },
+    { id: 10, marca: "Guild", modelo: "D-40", precio: 4000, cantidad: 1, img: "./img/GuildD40.jpg" },
+    { id: 11, marca: "Fender", modelo: "Jazz Bass", precio: 6000, cantidad: 1, img: "./img/FenderJazzBass.png" },
+    { id: 12, marca: "Fender", modelo: "Precision Bass", precio: 3500, cantidad: 1, img: "./img/FenderPrecisionBass.png" }
 ]
+
+document.addEventListener("keyup", e=>{
+
+  if (e.target.matches("#buscador")){
+
+      if (e.key === "Escape")e.target.value = ""
+
+      document.querySelectorAll(productos).forEach(producto =>{
+          producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?producto.classList.remove("filtro")
+            :producto.classList.add("filtro")
+      })
+  }
+})
 
 const pintarCarrito = () => {
     modalContainer.innerHTML = "";
     modalContainer.style.display = "flex";
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header";
-    modalHeader.innerHTML = `<h1 class="modal-header-title">Carrito.</h1>`;
+    modalHeader.innerHTML = `<h1 class="modal-header-title">Carrito</h1>`;
     modalContainer.append(modalHeader);
   
     const modalbutton = document.createElement("h1");
